cec2526075603e52b6413b706cf9b0947b5e2daf
wathika-eng <wathika02@gmail.com>
2024-10-04 17:19:26 +0300

    fix

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,13 +1,3 @@
-const getResponseFromAPI = () =>
-  new Promise((resolve, reject) => {
-    setTimeout(() => {
-      const success = true;
-      if (success) {
-        resolve("API response");
-      } else {
-        reject(new Error("API call failed"));
-      }
-    }, 1000);
-  });
-
-export default getResponseFromAPI;
+export default function getResponseFromAPI() {
+  return new Promise(() => {});
+}
