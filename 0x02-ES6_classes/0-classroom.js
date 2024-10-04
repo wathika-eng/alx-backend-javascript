ae47eba963752bcda59fddc168b3733c9d72c1c3
wathika-eng <wathika02@gmail.com>
2024-10-04 19:09:14 +0300

    jsClasses

---
+++ 0x02-ES6_classes/0-classroom.js
@@ -0,0 +1,10 @@
+class ClassRoom {
+    constructor(_maxStudentsSize){
+        this._maxStudentsSize = _maxStudentsSize
+    }
+    room(){
+        return this._maxStudentsSize
+    }
+}
+
+export default ClassRoom
