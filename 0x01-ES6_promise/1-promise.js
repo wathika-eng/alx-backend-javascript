b19eff86ea591b9d63cb8f724d7c848d283dee1b
wathika-eng <wathika02@gmail.com>
2024-10-04 17:58:24 +0300

    semi

--- 0x01-ES6_promise/1-promise.js
+++ 0x01-ES6_promise/1-promise.js
@@ -1,14 +1,15 @@
-const getFullResponseFromAPI = success => {
-  return new Promise((resolve, reject) => {
-    if (success === true) {
-      resolve({
-        status: 200,
-        body: 'success'
-      })
-    } else {
-      reject(new Error('The fake API is not working currently'))
-    }
-  })
-}
-
-export default getFullResponseFromAPI
+const getFullResponseFromAPI = (success) => {
+    return new Promise((resolve, reject) => {
+      if (success === true) {
+        resolve({
+          status: 200,
+          body: 'success'
+        });
+      } else {
+        reject(new Error('The fake API is not working currently'));
+      }
+    });
+  };
+  
+  export default getFullResponseFromAPI;
+  
