2bd02c83f3ded92d3f15f9db25c1d7f4615feda8
wathika-eng <wathika02@gmail.com>
2024-10-10 00:00:56 +0300

    assignment_2

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -1,3 +1,4 @@
+// create a teacher interface with  optional extra property
 interface Teacher {
   firstName: string;
   lastName: string;
@@ -7,18 +8,51 @@ interface Teacher {
   [propName: string]: any;
 }
 
+// create a director interface with the teacher interface as an extension
 interface Directors extends Teacher {
   numberOfReports: number;
 }
 
+// create a printTeacher function interface
 interface printTeacherFunction {
   (firstName: string, lastName: string): string;
 }
 
+// implement the printTeacher function
 export const printTeacher: printTeacherFunction = function (
   firstName: string,
   lastName: string,
 ): string {
   return `${firstName[0]}. ${lastName}`;
 };
-console.log(printTeacher("John", "Doe"));
+
+// constructor interface for the Student class
+interface StudentClassConstructor {
+  firstName: string;
+  lastName: string;
+}
+
+// interface to implement the Student class
+interface StudentClassInterface {
+  workOnHomework(): string;
+  displayName(): string;
+}
+
+// create a Student class
+class StudentClass implements StudentClassInterface {
+  // properties
+  firstName: string;
+  lastName: string;
+
+  constructor({ firstName, lastName }: StudentClassConstructor) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+  }
+  // method that returns a string
+  workOnHomework(): string {
+    return "Currently working";
+  }
+  displayName(): string {
+    return this.firstName;
+  }
+}
